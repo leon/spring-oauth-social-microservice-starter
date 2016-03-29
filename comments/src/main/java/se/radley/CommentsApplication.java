76ac4df7446f59cce43238afa67bb0bd675a8933
Leon Radley <leon@radley.se>
2016-03-29 06:42:47 +0200

    comments

---
+++ comments/src/main/java/se/radley/CommentsApplication.java
@@ -0,0 +1,12 @@
+package se.radley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsApplication.class, args);
+	}
+}
